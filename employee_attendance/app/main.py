b258dfce3bef58f1f66efb38b8d0994fe0a8ff29
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-09-04 17:42:30 +0530

    pyth pro

--- employee_attendance/app/main.py
+++ employee_attendance/app/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
 from . import models
+from fastapi.middleware.cors import CORSMiddleware
 from .database import engine
 from .routers import employee, attendance
 
 models.Base.metadata.create_all(bind=engine)
 
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # Your frontend origin
+    "http://127.0.0.1:5173",  # Alternative frontend origin
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows requests from your frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 app.include_router(employee.router)
 app.include_router(attendance.router)
