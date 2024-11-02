6e11eb797cd98954fef63b9e6cdaa360db040ac4
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-11-02 14:47:53 +0530

    updated more

--- employee_attendance/tests/conftest.py
+++ employee_attendance/tests/conftest.py
@@ -18,115 +18,108 @@ print("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-testingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-
-
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
 
 
 @pytest.fixture()
 def session():
+    print("my session fixture ran")
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-    db = testingSessionLocal()
+    db = TestingSessionLocal()
     try:
         yield db
     finally:
         db.close()
-    
+
 
 @pytest.fixture()
-def client (session):
-    def overrid_get_db():
+def client(session):
+    def override_get_db():
 
-     try:
-          yield session
-     finally:
-        session.close()
-    app.dependency_overrides[get_db] = overrid_get_db
-
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
 
 
 @pytest.fixture
 def test_user2(client):
-      user_data = {"email":"aaa495666@gmail.com",
-    "password":"hjhgf"}
-      res = client.post("/users/", json=user_data)
+    user_data = {"email": "sanjeev123@gmail.com",
+                 "password": "password123"}
+    res = client.post("/users/", json=user_data)
 
-      assert res.status_code == 201
-      print(res.json())
-      new_user = res.json()
-      new_user['password'] = user_data['password']
-      return new_user
+    assert res.status_code == 201
 
-
-
-
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
 
 
 @pytest.fixture
 def test_user(client):
-      user_data = {"email":"aaa4956@gmail.com",
-    "password":"hjhgf"}
-      res = client.post("/users/", json=user_data)
+    user_data = {"email": "sanjeev@gmail.com",
+                 "password": "password123"}
+    res = client.post("/users/", json=user_data)
 
-      assert res.status_code == 201
-      print(res.json())
-      new_user = res.json()
-      new_user['password'] = user_data['password']
-      return new_user
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
 
 @pytest.fixture
 def token(test_user):
-    return create_access_token({"user_id":test_user['id']})
+    return create_access_token({"user_id": test_user['id']})
+
 
 @pytest.fixture
 def authorized_client(client, token):
-     client.headers = {
-         **client.headers,
-         "Authorization": f"Bearer {token}"
-     }
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
 
-     return client
+    return client
+
 
 @pytest.fixture
-def test_posts(test_user, session ,test_user2):
+def test_posts(test_user, session, test_user2):
     posts_data = [{
-        "title":"first title",
-        "content":"first content",
-        "owner_id":test_user['id']
+        "title": "first title",
+        "content": "first content",
+        "owner_id": test_user['id']
     }, {
-        "title":"2nd title",
-        "content":"2nd content",
-        "owner_id":test_user['id']
-    }, 
-       {
-        "title":"3rd title",
-        "content":"3rd content",
-        "owner_id":test_user['id']
-       },
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id": test_user['id']
+    },
         {
-        "title":"3rd title",
-        "content":"3rd content",
-        "owner_id":test_user2['id']
-    
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user['id']
+    }, {
+        "title": "3rd title",
+        "content": "3rd content",
+        "owner_id": test_user2['id']
     }]
 
-
     def create_post_model(post):
-        return models.post(**post)
+        return models.Post(**post)
+
     post_map = map(create_post_model, posts_data)
     posts = list(post_map)
 
     session.add_all(posts)
-
-    # session.add_all([models.User(title="first title", content="first content",owner_id= test_user['id']),
-    #                 models.post(title="2nd content", content="2nd content", owner_id= test_user['id']),  models.post(title="3rd title", content="3rd content", owner_id= test_user['id'])])
+    # session.add_all([models.Post(title="first title", content="first content", owner_id=test_user['id']),
+    #                 models.Post(title="2nd title", content="2nd content", owner_id=test_user['id']), models.Post(title="3rd title", content="3rd content", owner_id=test_user['id'])])
     session.commit()
 
-    posts = session.query(models.post).all()
+    posts = session.query(models.Post).all()
     print("111111111111111111111111111111111")
     print(posts)
     print(posts[0].__dict__)
